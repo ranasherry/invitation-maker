7dd8757b2a39b2d88d3c3973932efc306e38879d
ranasherry <ranasherry94@gmail.com>
2024-06-21 14:06:59 +0500

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:invitation_maker/app/routes/app_pages.dart';
 import 'package:invitation_maker/app/theme/app_theme.dart';
+import 'package:invitation_maker/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+  MyApp({super.key});
+// observer.analytics.setAnalyticsCollectionEnabled(true);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    observer.analytics.setAnalyticsCollectionEnabled(kReleaseMode);
+
     return ScreenUtilInit(
         minTextAdapt: true,
         designSize: const Size(1428, 2000),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
+import firebase_messaging
+import firebase_remote_config
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
